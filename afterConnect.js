3ce711197301ec9fb8bb839e89becbed0a9457c7
enrate <80484441+enrate@users.noreply.github.com>
2025-04-29 09:14:14 +0300

    Update afterConnect.js

--- afterConnect.js
+++ afterConnect.js
@@ -12,34 +12,40 @@ async function processConnectedPlayer(player, identity) {
                 [identity]
             );
             if (checkCreatedResults.length === 0) {
-                // 1. Создаем запись подключения
-                const [connectionResult] = await connection.query(
-                    `INSERT INTO player_connections 
-                    (timestamp_first_connection, timestamp_last_connection)
-                    VALUES (NOW(), NOW())`
-                );
+                // // 1. Создаем запись подключения
+                // const [connectionResult] = await connection.query(
+                //     `INSERT INTO player_connections 
+                //     (timestamp_first_connection, timestamp_last_connection)
+                //     VALUES (NOW(), NOW())`
+                // );
                 
-                const connectionId = connectionResult.insertId;
+                // const connectionId = connectionResult.insertId;
                 
-                const [infoResult] = await connection.query(
-                    `INSERT INTO players_info 
-                    (connection_id, player_id, player_name)
-                    VALUES (?, ?, ?)`,
-                    [connectionId, identity, player]
-                );
+                // const [infoResult] = await connection.query(
+                //     `INSERT INTO players_info 
+                //     (connection_id, player_id, player_name)
+                //     VALUES (?, ?, ?)`,
+                //     [connectionId, identity, player]
+                // );
                 
-                const [statsResult] = await connection.query(
-                    `INSERT INTO players_stats (player_id)
-                    VALUES (?)
-                    ON DUPLICATE KEY UPDATE player_id = VALUES(player_id)`,
-                    [identity]
-                );
+                // const [statsResult] = await connection.query(
+                //     `INSERT INTO players_stats (player_id)
+                //     VALUES (?)
+                //     ON DUPLICATE KEY UPDATE player_id = VALUES(player_id)`,
+                //     [identity]
+                // );
                 
+                // await connection.query(
+                //     `UPDATE players_info
+                //     SET stats_id = ?
+                //     WHERE id = ?`,
+                //     [statsResult.insertId, infoResult.insertId]
+                // );
                 await connection.query(
-                    `UPDATE players_info
-                    SET stats_id = ?
-                    WHERE id = ?`,
-                    [statsResult.insertId, infoResult.insertId]
+                    `INSERT INTO temp_player_events 
+                    (player_id, player_name, event_type)
+                    VALUES (?, ?, 'connect')`,
+                    [identity, player]
                 );
                 
             } else {
